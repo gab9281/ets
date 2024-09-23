5040c2189ddd72b09ea4764973b7ac8128489360
MathieuSevignyLavallee <89943988+MathieuSevignyLavallee@users.noreply.github.com>
2024-09-22 21:04:07 -0400

    jwt auth for route auth

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -4,6 +4,6 @@ const jwt = require('../middleware/jwtToken.js');
 
 const authController = require('../controllers/auth.js')
 
-router.get("/getActiveAuth", authController.getActive);
+router.get("/getActiveAuth",jwt.authenticate, authController.getActive);
 
 module.exports = router;
