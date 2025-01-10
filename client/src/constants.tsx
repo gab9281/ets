51d3d9f4731bf0d5b8fadaea4e34960fce30c544
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2025-01-10 15:45:07 -0500

    got back to import.meta (so dev version works)

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -1,8 +1,8 @@
 // constants.tsx
 const ENV_VARIABLES = {
     MODE: 'production',
-    VITE_BACKEND_URL: process.env.VITE_BACKEND_URL || "",
-    VITE_BACKEND_SOCKET_URL: process.env.VITE_BACKEND_SOCKET_URL || "",
+    VITE_BACKEND_URL: import.meta.env.VITE_BACKEND_URL || "",
+    VITE_BACKEND_SOCKET_URL: import.meta.env.VITE_BACKEND_SOCKET_URL || "",
 };
 
 console.log(`ENV_VARIABLES.VITE_BACKEND_URL=${ENV_VARIABLES.VITE_BACKEND_URL}`);
