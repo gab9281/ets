3366fbe18cf0c878fed69255c4b9518b1f4874a0
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2025-01-10 15:42:47 -0500

    map the constants import to a mocked one

---
+++ client/src/__mocks__/constantsMock.tsx
@@ -0,0 +1,13 @@
+console.log('constantsMock.tsx is loaded');
+
+// constants.tsx
+const ENV_VARIABLES = {
+    MODE: 'production',
+    VITE_BACKEND_URL: process.env.VITE_BACKEND_URL || "",
+    VITE_BACKEND_SOCKET_URL: process.env.VITE_BACKEND_SOCKET_URL || "",
+};
+
+console.log(`ENV_VARIABLES.VITE_BACKEND_URL=${ENV_VARIABLES.VITE_BACKEND_URL}`);
+console.log(`ENV_VARIABLES.VITE_BACKEND_SOCKET_URL=${ENV_VARIABLES.VITE_BACKEND_SOCKET_URL}`);
+
+export { ENV_VARIABLES };
