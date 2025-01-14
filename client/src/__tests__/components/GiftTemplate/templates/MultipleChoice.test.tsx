89dd55ca8f79e6bbf4511dc0b9ca03ca0b1a9683
JubaAzul <118773284+JubaAzul@users.noreply.github.com>
2025-01-13 20:03:12 -0500

    try import react inside class

--- client/src/__tests__/components/GiftTemplate/templates/MultipleChoice.test.tsx
+++ client/src/__tests__/components/GiftTemplate/templates/MultipleChoice.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MultipleChoice } from '../../../../components/GiftTemplate/templates';
--- client/src/__tests__/components/GiftTemplate/templates/ShortAnswer.test.tsx
+++ client/src/__tests__/components/GiftTemplate/templates/ShortAnswer.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ShortAnswer from '../../../../components/GiftTemplate/templates/ShortAnswer';
