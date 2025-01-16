b46e2e3985943e3460923a1c58496e5b9f48eeb9
JubaAzul <118773284+JubaAzul@users.noreply.github.com>
2025-01-16 10:40:27 -0500

    removed import in every class, try to update react

--- client/src/__tests__/components/GiftTemplate/templates/Numerical.test.tsx
+++ client/src/__tests__/components/GiftTemplate/templates/Numerical.test.tsx
@@ -1,5 +1,3 @@
-// @ts-expect-error : Required for pipeline compatibility
-import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Numerical from '../../../../components/GiftTemplate/templates/Numerical';
--- client/src/__tests__/components/GiftTemplate/templates/ShortAnswer.test.tsx
+++ client/src/__tests__/components/GiftTemplate/templates/ShortAnswer.test.tsx
@@ -1,5 +1,3 @@
-// @ts-expect-error: Required for pipeline compatibility
-import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ShortAnswer from '../../../../components/GiftTemplate/templates/ShortAnswer';
